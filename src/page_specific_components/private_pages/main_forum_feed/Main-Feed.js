3e4271538f92fbb11b6b3c0e9f49a69ec78d0ff6
Ryan Gambrell <rgambrell@ggc.edu>
2022-03-15 16:54:35 -0400

    Site Structure Update: Splintered Main-Feed into Main-Feed and Post-Creation so that creation of posts is handled on a new page.

--- src/page_specific_components/private_pages/main_forum_feed/Main-Feed.js
+++ src/page_specific_components/private_pages/main_forum_feed/Main-Feed.js
@@ -10,39 +10,10 @@ class MainFeed extends Component {
     constructor(props) {
         super(props);
 
-        //Bindings
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onDescriptionChange = this.onDescriptionChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
         this.state =
         {
-            title: '',
-            description: '',
             feedPosts: []
         }
-    }
-
-    onTitleChange(e) {
-        this.setState({
-            title: e.target.value
-        });
-    }
-
-    onDescriptionChange(e) {
-        this.setState({
-            description: e.target.value
-        });
-    }
-
-    onSubmit(e) {
-        e.preventDefault();
-
-        createPost({
-            poster: "rgambrell@ggc.edu",
-            title: this.state.title,
-            description: this.state.description
-        });
     }
 
     componentDidMount(){
@@ -62,19 +33,18 @@ class MainFeed extends Component {
                     {
                         [
                             {
-                                'text': "Home",
-                                'link': "/"
-                            }
+                                'text': "Profile",
+                                'link': "/User_Profile"
+                            },
+                            {
+                                'text': "New Post",
+                                'link': "/Post_Creation"
+                            },
                         ]
                     }
                 />
                 <div className={bodyStyles.ScrollingContent}>
                     <div className={bodyStyles.PageBody}>
-                        <form onSubmit={this.onSubmit}>
-                            <input type="text" placeholder="Add Title Here" onChange={this.onTitleChange}/><br/>
-                            <input type="text" placeholder="Add Description Here" onChange={this.onTitleChange}/><br/>
-                            <input type="submit" name="Update" value="Update"/>
-                        </form>
                         {
                             this.state.feedPosts.map(post => <ForumPost poster={post.poster} title={post.title} 
                                 description={post.description}/>)
@@ -82,9 +52,7 @@ class MainFeed extends Component {
                     </div>
                     <AppFooter />
                 </div>
-
             </Fragment>
-
         );
     }
 }
---
+++ src/page_specific_components/private_pages/post_creation/Post-Creation.js
@@ -0,0 +1,72 @@
+import { Component, Fragment } from "react";
+import bodyStyles from '../../../shared_site_css/body_styles/internal-body.module.css';
+import styles from './post-creation.module.css';
+import AppHeader from '../../../shared_site_components/page-header/header-and-navebar';
+import AppFooter from '../../../shared_site_components/page-footer/footer';
+import { createPost } from '../../../firebase/ops/post';
+
+class PostCreation extends Component {
+    constructor(props) {
+        super(props);
+
+        //Bindings
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onDescriptionChange = this.onDescriptionChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state =
+        {
+            title: '',
+            description: '',
+            feedPosts: []
+        }
+    }
+
+    onTitleChange(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+
+    onDescriptionChange(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        createPost({
+            poster: "rgambrell@ggc.edu",
+            title: this.state.title,
+            description: this.state.description
+        });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <AppHeader navBarContents=
+                    {
+                        [
+                        ]
+                    }
+                />
+                <div className={bodyStyles.ScrollingContent}>
+                    <div className={bodyStyles.PageBody}>
+                        <form onSubmit={this.onSubmit}>
+                            <input type="text" placeholder="Add Title Here" onChange={this.onTitleChange}/><br/>
+                            <input type="text" placeholder="Add Description Here" onChange={this.onTitleChange}/><br/>
+                            <input type="submit" name="Update" value="Update"/>
+                        </form>
+                    </div>
+                    <AppFooter />
+                </div>
+
+            </Fragment>
+        );
+    }
+}
+
+export default PostCreation;
