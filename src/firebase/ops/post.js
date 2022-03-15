61a22f3ca88bf5ab4f06d15badfcb28c16a85cbb
Ryan Gambrell <rgambrell@ggc.edu>
2022-03-15 15:55:46 -0400

    Posting Operations Update: Modified post fetching operation to return a list of posts rather than a Firebase-specific snapshot. In addition, the create post operation was updated to ensure that the operation references the 'posts' collection and actively stores the description.

--- src/firebase/ops/post.js
+++ src/firebase/ops/post.js
@@ -5,14 +5,24 @@ import { doc, collection, getDocs, setDoc } from "firebase/firestore";
 export async function fetchAllPosts(){
 
     const postsSnapshot = await getDocs(collection(db, "posts"));
-    return postsSnapshot; 
+    
+    const posts = [];
+    postsSnapshot.forEach((doc) => {
+        posts.push({
+            poster: doc.data().poster,
+            title: doc.data().title, 
+            description: doc.data().description,
+        })
+      });
+    
+    return posts; 
 }
 
 export async function createPost(postDetails){
-    const postRef = doc(db, "posts");
+    const postRef = doc(collection(db, "posts"));
     await setDoc(postRef, {
         poster: postDetails.poster,
         title: postDetails.title, 
-        description: userDetails.description,
+        description: postDetails.description,
     });
 }
