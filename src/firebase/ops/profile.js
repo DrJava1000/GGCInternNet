64008ae597c0753844d5f21fd77c57a32ef7597c
Ryan Gambrell <rgambrell@ggc.edu>
2022-03-15 02:26:39 -0400

    Firebase Profile Operations Update: Modified Profile-loading function to fetch picture based on name stored in Fire Store. An update profile function has also been added for the purposes of ensuring that user information, profile pictures, and resumes, are uploaded to Firebase.

--- src/firebase/ops/profile.js
+++ src/firebase/ops/profile.js
@@ -1,7 +1,7 @@
 import { db } from '../firebase_init'; 
 
-import { doc, getDoc } from "firebase/firestore"; 
-import { getStorage, ref, getDownloadURL} from "firebase/storage";
+import { doc, getDoc, setDoc } from "firebase/firestore"; 
+import { getStorage, ref, getDownloadURL, uploadBytes} from "firebase/storage";
 
 export async function loadUserProfile(id){
     const docRef = doc(db, "profiles", "" + id);
@@ -16,19 +16,21 @@ export async function loadUserProfile(id){
         const userDetails = {
             name: firestoreProfInfo.name,
             major: firestoreProfInfo.major,
-            concentration: firestoreProfInfo.concentration
+            concentration: firestoreProfInfo.concentration,
+            picFileName : firestoreProfInfo.picFileName,
+            resumeFileName: firestoreProfInfo.resumeFileName
         }
         
         const storage = getStorage();
         
         // Get download link for profile pic
-        await getDownloadURL(ref(storage, 'profile_pics/' + id + '/profile_pic.png'))
+        await getDownloadURL(ref(storage, 'profile_pics/' + id + '/' + userDetails.picFileName))
             .then((url) => {
                 userDetails.pic = url; 
             })
         
         // Get download link for 
-        await getDownloadURL(ref(storage, "resumes/" + id + "/resume.docx"))
+        await getDownloadURL(ref(storage, "resumes/" + id + "/" + userDetails.resumeFileName))
             .then((url) => {
                 userDetails.resume = url;
             })  
@@ -38,4 +40,31 @@ export async function loadUserProfile(id){
     } else {
         console.log("No such document!");
     }
+}
+
+export async function updateUserProfile(userDetails){
+    const profileRef = doc(db, "profiles", userDetails.id)
+    await setDoc(profileRef, {
+        name: userDetails.name, 
+        major: userDetails.major,
+        concentration: userDetails.concentration,
+        picFileName : userDetails.pic.name,
+        resumeFileName: userDetails.resume.name
+    }, { 
+        merge: true 
+    });
+
+    console.log("Profile Updated");
+
+    const storage = getStorage();
+    const picRef = ref(storage, 'profile_pics/' + userDetails.id + '/' + userDetails.pic.name);
+    const resumeRef = ref(storage, "resumes/" + userDetails.id + "/" + userDetails.resume.name);
+
+    await uploadBytes(picRef, userDetails.pic).then((snapshot) => {
+        console.log('Uploaded a profile pic!');
+    });
+
+    await uploadBytes(resumeRef, userDetails.resume).then((snapshot) => {
+        console.log('Uploaded a resume!');
+    });
 }
