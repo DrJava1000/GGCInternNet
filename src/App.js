f2a2b7f7a65b674aaff711fd5cdc6c1370403207
Ryan Gambrell <rgambrell@ggc.edu>
2022-03-14 20:17:47 -0400

    App.js Routing Update: Updated App.js routing to make Profile Template routable.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './firebase/firebase_init';
+
+// Public Page Imports
 import Portal from './page_specific_components/public_pages/login_portal/Portal';
 import Signup from './page_specific_components/public_pages/signup/User-Provisioning'; 
+
+// Private Page Imports 
 import InternalPage from './page_specific_components/private_pages/InternalPage';
+
+import Profile from './page_specific_components/private_pages/user_profile/User-Profile';
+
 import {Route, Routes} from 'react-router-dom'
+
 import './App.css';
 
 function App() {
@@ -18,6 +26,9 @@ function App() {
 
         {/* Template Internal Page */ }
         <Route exact path="/InternalPage" element={<InternalPage />}/> 
+
+        {/* User Profile Page */}
+        <Route exact path="/User_Profile" element={<Profile />}/> 
       </Routes>
     </main>
   );
