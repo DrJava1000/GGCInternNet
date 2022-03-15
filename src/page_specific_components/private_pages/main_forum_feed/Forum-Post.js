e58dd2dcb4a58624b6444a53aa01d8c5e9541dd0
Ryan Gambrell <rgambrell@ggc.edu>
2022-03-15 14:38:13 -0400

    Main Feed Update: Separate Template Component Added for Individual Forum Posts.

---
+++ src/page_specific_components/private_pages/main_forum_feed/Forum-Post.js
@@ -0,0 +1,29 @@
+import { Component, Fragment } from "react";
+import styles from './main-feed.module.css';
+
+class ForumPost extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state =
+        {
+            poster: props.poster,
+            title: props.title,
+            description: props.description
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <h3>{this.state.title}</h3>
+                    <h5>{this.state.poster}</h5>
+                    <p>{this.state.description}</p>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default ForumPost;
