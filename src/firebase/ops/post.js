ee840a8701efd71b4fcb801c60c848bebda07a85
Ryan Gambrell <rgambrell@ggc.edu>
2022-04-19 12:59:21 -0400

    Post Ops Update: Updated Fetch All Posts operation to add-in support for company and major/concentration search filters.

--- src/firebase/ops/post.js
+++ src/firebase/ops/post.js
@@ -2,41 +2,76 @@ import { db } from "../firebase_init";
 
 import { 
   doc, 
+  documentId,
   collection, 
   getDocs, 
   setDoc, 
-  getDoc, 
   where, 
   query, 
   orderBy,
   startAt,
   endAt,
+  limit,
   deleteDoc 
 } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL, uploadBytes } from "firebase/storage";
 
 // MAIN FEED: FETCH ALL POSTS
-export async function fetchAllPosts() {
-  const postsSnapshot = await getDocs(collection(db, "posts"));
+export async function fetchAllPosts(filterOptions) {
+  
+  // Post Collection reference
+  const postsRef = collection(db, "posts");
+  // Post Collection Query
+  var postsQuery;
+
+  // Create a post query that searches for all posts
+  // If a company filter has been applied, filter for company name
+  switch(filterOptions.filterType){
+    case 'company': 
+      postsQuery = query(postsRef, orderBy("company"), startAt(filterOptions.companySearchQuery), 
+      endAt(filterOptions.companySearchQuery + '\uf8ff')); break;
+    default: 
+      postsQuery = query(postsRef); 
+  }
+  
+  // Search for all posts using applied filters (if any)
+  const postsSnapshot = await getDocs(postsQuery);
+
+  // List of posts to return
   const posts = [];
   
-  // get all posts 
+  // Loop through all retrieved posts, get poster's name from the userID in post,
+  // and inject it into each post before it's returned
   for(const post of postsSnapshot.docs) {
-    // get poster's name from the ID stored in the post
-    const profileRef = doc(db, "profiles", "" + post.data().userId);
-    const profileSnap = await getDoc(profileRef);
-    var name;
+  
+    // Profile Collection reference
+    const profileRef = collection(db, "profiles");
+    // Profile Collection query
+    var profileQuery; 
 
-    if (profileSnap.exists()) {
-      name = profileSnap.data().name;
-    } else {
-      console.log("No such document!");
+    // If major/concentration filter has been applied, 
+    // use those when pulling the user's profile
+    if(filterOptions.filterType === 'major/concentration'){
+      profileQuery = query(profileRef, where(documentId(), "==", post.data().userId), 
+      where("major", "==", filterOptions.major), 
+      where("concentration", "==", filterOptions.concentration));
+    }else{
+      profileQuery = query(profileRef, where(documentId(), "==", post.data().userId), limit(1)); 
     }
 
-    // add each post to a list
-    posts.push({
-      ...post.data(), posterName: name, id: post.id
-    });
+    // Get user attached to post.
+    // While the query will only actually bring back one 
+    // attached profile (due to unique id), we still have to retrieve
+    // using getDocs.
+    const profileSnap = await getDocs(profileQuery);
+
+    if (!profileSnap.empty) {
+      // Add the post and inject the corresponding poster's name
+      // and only do so if the profile passes all filters.
+      posts.push({
+        ...post.data(), posterName: profileSnap.docs[0].data().name, id: post.id
+      });
+    }
   };
 
   // return list of posts
