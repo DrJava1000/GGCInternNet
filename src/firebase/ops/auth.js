1a6fa293b1204a147fab404f696046366e9e824b
Ryan Gambrell <rgambrell@ggc.edu>
2022-05-01 22:22:08 -0400

    Auth/Profile Ops Update: Added a 'getRole' operation that can be used to fetch a user's system role from Firebase. This operation was chained to the backend 'login' operation so that we can load both the user's id and role when they sign-in.

--- src/firebase/ops/auth.js
+++ src/firebase/ops/auth.js
@@ -5,14 +5,19 @@ import
     signInWithEmailAndPassword, 
     signOut
 } from "firebase/auth";
-import { createUserProfile } from "./profile";
+import { createUserProfile, getRole } from "./profile";
 
 export const appAuthInstance = getAuth();
 
+// CREATE AN ACCOUNT
 export async function createAccount(email, password, name)
 {
     createUserWithEmailAndPassword(appAuthInstance, email, password)
     .then((userCredential) => {
+        // For new accounts,
+        // specify an associated user profile 
+        // with the new user's id, name, and a
+        // default major and concentration.
         createUserProfile({
             id: userCredential.user.uid,
             name: name,
@@ -27,18 +32,33 @@ export async function createAccount(email, password, name)
     });
 }
 
+// LOGIN A USER
 export async function login(email, password)
 {
-    signInWithEmailAndPassword(appAuthInstance, email, password)
-    .then((userCredential) => {
-        console.log("User " + userCredential.user.email + " has been signed in successfully."); 
+    var userInfo = {};
+
+    await signInWithEmailAndPassword(appAuthInstance, email, password)
+    .then(async (userCredential) => {
+        // Obtain user's id from here
+        userInfo.id = userCredential.user.uid;
+        // Fetch user's role
+        // Wait to ensure that account role can be retrieved
+        await getRole(userCredential.user.uid).then(role => {
+            userInfo.role = role;
+            console.log("User " + userCredential.user.email + " has been signed in successfully."); 
+        }).catch(error => {
+            console.log(error); 
+        })
     })
     .catch((error) => {
         console.log("You couldn't be signed in. See below");
         console.log(error);
     });
+
+    return userInfo;
 }
 
+// LOGOUT USER
 export async function logout()
 {
     signOut(appAuthInstance)
--- src/firebase/ops/profile.js
+++ src/firebase/ops/profile.js
@@ -4,6 +4,9 @@ import { doc, getDoc, setDoc } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL, uploadBytes} from "firebase/storage";
 
 // CREATE USER PROFILE
+// Wrapper for updateUserProfile (for naming convenience)
+// The newAccount attribute is used to let updateUserProfile know
+// that incoming information is for a new account. 
 export async function createUserProfile(userDetails){
     userDetails.newAccount = true; 
     updateUserProfile(userDetails);
@@ -12,6 +15,9 @@ export async function createUserProfile(userDetails){
 // UPDATE USER PROFILE
 export async function updateUserProfile(userDetails){
     const profileRef = doc(db, "profiles", userDetails.id)
+    
+    // Update name, major, and concentration
+    // on account first.
     await setDoc(profileRef, {
         name: userDetails.name, 
         major: userDetails.major,
@@ -30,23 +36,27 @@ export async function updateUserProfile(userDetails){
     if(userDetails.newAccount)
     {
         // for new accounts, use default logo and resume
+        // that are stored in Firebase already
         logoStoragePath = "profile_pics/default/grizzly_logo.PNG";
         resumeStoragePath = "resumes/default/resume_template.docx";
         
         picRef = ref(storage, logoStoragePath);
         resumeRef = ref(storage, resumeStoragePath);
         
+        // Link to default profile picture
         await setDoc(profileRef, {
             picFileName : logoStoragePath
         }, { 
             merge: true 
         });
 
+        // Link to default resume
         await setDoc(profileRef, {
             resumeFileName: resumeStoragePath
         }, { 
             merge: true 
         });
+    // UPDATE AN ALREADY-EXISTING ACCOUNT
     }else{
         logoStoragePath = 'profile_pics/' + userDetails.id + '/' + userDetails.pic.name;
         resumeStoragePath = "resumes/" + userDetails.id + "/" + userDetails.resume.name;
@@ -54,6 +64,8 @@ export async function updateUserProfile(userDetails){
         picRef = ref(storage, logoStoragePath);
         resumeRef = ref(storage, resumeStoragePath);
         
+        // If a new profile picture is being uploaded, proceed
+        // but don't assume that a picture is always being updated
         if(userDetails.pic.name !== undefined){
             await uploadBytes(picRef, userDetails.pic).then((snapshot) => {
                 console.log('Uploaded a profile pic!');
@@ -66,6 +78,8 @@ export async function updateUserProfile(userDetails){
             });
         }
 
+        // If a new resume is being uploaded, proceed
+        // but don't assume that a resume is always being updated
         if(userDetails.resume.name !== undefined){
             await uploadBytes(resumeRef, userDetails.resume).then((snapshot) => {
                 console.log('Uploaded a resume!');
@@ -89,6 +103,7 @@ export async function loadUserProfile(id){
         
         const firestoreProfInfo = docSnap.data(); 
         
+        // Get name, major, and concentration
         const userDetails = {
             name: firestoreProfInfo.name,
             major: firestoreProfInfo.major,
@@ -97,17 +112,42 @@ export async function loadUserProfile(id){
         
         const storage = getStorage();
         
+        // Create a downloadable link for the profile picture
+        // from the linked file name
+        // stored in the profile
         await getDownloadURL(ref(storage, firestoreProfInfo.picFileName))
             .then((url) => {
                 userDetails.pic = url; 
             })
         
+        // Create a downloadable link for the resume
+        // from the linked file name
+        // stored in the profile
         await getDownloadURL(ref(storage, firestoreProfInfo.resumeFileName))
             .then((url) => {
                 userDetails.resume = url;
             })  
         
         return userDetails; 
+
+    } else {
+        console.log("No such document!");
+    }
+}
+
+// FETCH USER ROLE (FROM PROFILE)
+// 
+// Firebase doesn't support user roles
+// and, thus, a user's role has to be saved
+// in Firestore and retrieved separately
+export async function getRole(id){
+    const docRef = doc(db, "profiles", "" + id);
+    const docSnap = await getDoc(docRef); 
+
+    if (docSnap.exists()) { 
+        
+        const firestoreProfInfo = docSnap.data(); 
+        return firestoreProfInfo.role; 
 
     } else {
         console.log("No such document!");
